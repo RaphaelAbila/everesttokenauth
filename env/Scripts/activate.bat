f8950b5fb5157873329e31fc9ac865445f9179bc
timothyisaiah <timothyisaiah7@gmail.com>
2021-08-29 21:51:39 +0300

    initiatl commit for deployable files

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Gigs\flask\Flask-Auth-REST-API\env
+set VIRTUAL_ENV=D:\Isaiah\Isaiah\Gigs\Everest Guage\everestpy\everesttokenauth\env
 
 if not defined PROMPT set PROMPT=$P$G
 
